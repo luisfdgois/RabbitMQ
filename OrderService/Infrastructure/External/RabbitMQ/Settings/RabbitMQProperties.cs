3cf585500724e72457d527eee29c71abcb1bca8e
Luis Fernando Gois <luisz.dantass@hotmail.com>
2022-05-29 00:07:23 -0300

    Defining connection properties with Rabbitmq

---
+++ OrderService/Infrastructure/External/RabbitMQ/Settings/RabbitMQProperties.cs
@@ -0,0 +1,13 @@
+﻿namespace Infrastructure.Services.Settings
+{
+    public class RabbitMQProperties
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string VirtualHost { get; set; }
+        public string Port { get; set; }
+        public string HostName { get; set; }
+
+        public string Uri { get { return $"amqp://{UserName}:{Password}@{HostName}:{Port}/{VirtualHost}"; } }
+    }
+}
