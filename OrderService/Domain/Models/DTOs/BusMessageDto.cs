edc055148f17dab81aae471014f386617ea40aa4
Luis Fernando Gois <luisz.dantass@hotmail.com>
2022-05-30 23:20:02 -0300

    Creating the Dto classes that will be sent as a message to rabbitMQ

---
+++ OrderService/Domain/Models/DTOs/BusMessageDto.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Models.Enums;
+
+namespace Domain.Models.DTOs
+{
+    public abstract class BusMessageDto
+    {
+        public virtual PaymentTypeDto PaymentType { get; }
+    }
+}
---
+++ OrderService/Domain/Models/DTOs/CreditCardMessageDto.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Models.DTOs;
+using Domain.Models.Enums;
+using System.Text.Json;
+
+namespace Domain.DTOs
+{
+    public class CreditCardMessageDto : BusMessageDto
+    {
+        public Guid OrderId { get; set; }
+        public string Number { get; private set; }
+        public string CVV { get; private set; }
+        public int NumberOfInstallment { get; private set; }
+        public decimal ValuePerInstallment { get; private set; }
+        public override PaymentTypeDto PaymentType { get => PaymentTypeDto.CreditCard; }
+
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+    }
+}
