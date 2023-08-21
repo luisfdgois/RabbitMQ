2db2e85decbe98c9c5e311f16a0f6eafb0e91a92
Luis Fernando Gois <luisz.dantass@hotmail.com>
2023-08-21 20:47:02 -0300

    Removing (temporary) unused code for Polly resilient connection and fixing RabbitMQ hostname

--- CreditCard/CreditCard/Bootstrapper.cs
+++ CreditCard/CreditCard/Bootstrapper.cs
@@ -1,10 +1,7 @@
 ﻿using CreditCard.RabbitMQ.Consumer;
 using CreditCard.RabbitMQ.Publisher;
 using CreditCard.RabbitMQ.Settings;
-using Polly;
 using RabbitMQ.Client;
-using RabbitMQ.Client.Exceptions;
-using System.Net.Sockets;
 
 namespace CreditCard
 {
@@ -21,19 +18,10 @@ namespace CreditCard
                         var connectionFactory = new ConnectionFactory { Uri = new Uri(rabbitMQProperties!.Uri) };
                         connectionFactory.ClientProvidedName = "app:CreditCard-Service";
 
-                        //Using Polly to establish a resilient connection
-                        //var connection = GetRabbitMQConnectionPolicy().Execute(() => { return connectionFactory.CreateConnection(); });
-                        //return connection;
-
                         return connectionFactory.CreateConnection();
                     });
 
             return services;
         }
-
-        private static Policy GetRabbitMQConnectionPolicy() => Policy.Handle<SocketException>()
-                                                                     .Or<BrokerUnreachableException>()
-                                                                     .WaitAndRetry(retryCount: 5, _ => TimeSpan.FromSeconds(10),
-                                                                     (exception, _, retryCount, context) => { Console.WriteLine($"Retry: {retryCount} | Exception: {exception.Message}"); });
     }
 }
