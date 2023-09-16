38c86459149839e1139cba65d4109243b4008d2c
Luis Fernando Gois <luisz.dantass@hotmail.com>
2023-09-16 16:47:42 -0300

    [OrderService] Creating custom exceptions

---
+++ OrderService/Domain/Exceptions/IncompatiblePublisherBusException.cs
@@ -0,0 +1,7 @@
+﻿namespace Domain.Exceptions
+{
+    public class IncompatiblePublisherBusException : Exception
+    {
+        public IncompatiblePublisherBusException(string messageType) : base($"This publisher bus is only compatible with the {messageType}") { }
+    }
+}
